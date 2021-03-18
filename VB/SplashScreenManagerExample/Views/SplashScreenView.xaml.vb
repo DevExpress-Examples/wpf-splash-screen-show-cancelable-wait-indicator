ff2bca2b9945fbe614651746594f3d29a178303c
DevExpressExampleBot <fakeEmail>
2021-03-18 14:10:42 +0300

    Source auto update [skip ci]

---
+++ VB/SplashScreenManagerExample/Views/SplashScreenView.xaml.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Windows.Controls
+
+Namespace SplashScreenManagerExample.Views
+	Partial Public Class SplashScreenView
+		Inherits UserControl
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
