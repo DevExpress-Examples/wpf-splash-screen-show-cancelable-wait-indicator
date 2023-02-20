3f6e0092ee6e8d5727ecd83c97d0849e2849b47b
DevExpressExampleBot <fakeEmail>
2023-02-20 15:16:27 +0400

    Source auto update [skip ci]

--- VB/SplashScreenManagerExample/Views/MainView.xaml.vb
+++ VB/SplashScreenManagerExample/Views/MainView.xaml.vb
@@ -1,26 +1,15 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace SplashScreenManagerExample.Views
-	''' <summary>
-	''' Interaction logic for View1.xaml
-	''' </summary>
-	Partial Public Class MainView
-		Inherits UserControl
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    ''' <summary>
+    ''' Interaction logic for View1.xaml
+    ''' </summary>
+    Public Partial Class MainView
+        Inherits UserControl
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 End Namespace
